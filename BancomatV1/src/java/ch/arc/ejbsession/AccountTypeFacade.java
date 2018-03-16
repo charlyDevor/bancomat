aa8941aeead458d3e72b3a4f2034335d6b6de10c
ombangnc <charlomb@yahoo.fr>
2018-03-16 05:20:29 +0100

    entity, controller, session

---
+++ BancomatV1/src/java/ch/arc/ejbsession/AccountTypeFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.arc.ejbsession;
+
+import ch.arc.entities.AccountType;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Ombiche
+ */
+@Stateless
+public class AccountTypeFacade extends AbstractFacade<AccountType> {
+
+    @PersistenceContext(unitName = "BancomatV1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AccountTypeFacade() {
+        super(AccountType.class);
+    }
+    
+}
